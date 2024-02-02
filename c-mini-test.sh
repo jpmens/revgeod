df5d17a419656a36adb7f55f5aa4ce922764d5ef
Jan-Piet Mens <jp@mens.de>
2024-02-02 22:14:57 +0100

    add support for REVGEO_ variables

--- c-mini-test.sh
+++ c-mini-test.sh
@@ -4,11 +4,15 @@
 # 4 Rue du Général Lambert, 75015 Paris, France
 lat=48.85593
 lon=2.29431
-url="http://127.0.0.1:8865/rev?lat=${lat}&lon=${lon}&app=c-mini-test"
+
+host=${REVGEO_HOST:="127.0.0.1"}
+port=${REVGEO_PORT:=8865}
+
+url="http://$host:$port/rev?lat=${lat}&lon=${lon}&app=c-mini-test"
 
 echo $url
 
-json=$(curl  -sS $url)
+json=$(curl -sSf $url)
 
 str=$(echo "$json" | jq -r .address.village)
 
